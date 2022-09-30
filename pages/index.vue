f318246ff30964c4e62b36079d8247ac55fee929
domi <dominik.ziegenhagel@gmail.com>
2022-09-30 13:38:07 +0200

    :sparkles: Remove app.vue and create pages/ dir, so nuxt enables routing. Created 2 test pages as well

---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <!-- have a wrapper, bcuz template only may have one child -->
+    <div>
+        <h1>Hallo Welt</h1>
+        <a href="/page2">Go to page 2</a>
+    </div>
+</template>
---
+++ pages/page2.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Page twoo</h1>
+</template>
